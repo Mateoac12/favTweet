249e2dd6418671730891a22a889c6d8919498ec9
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-22 16:56:33 -0300

    feat: create item in a list result

---
+++ src/components/AutocompleteItem/index.tsx
@@ -0,0 +1,26 @@
+import { ITargetTweet } from '../../types'
+import { generateExternalTwitterLink } from '../../utils/generateExternalTwitterLink'
+
+interface Props extends ITargetTweet {}
+
+const AutocompleteItem = ({ name, username, text, avatar, tweetId }: Props) => {
+  return (
+    <a
+      href={generateExternalTwitterLink({ username, tweetId })}
+      target="_blank"
+      className="grid search-result-grid bg-transparent hover:bg-slate-200/40 transition-colors p-2 rounded-lg divide-x"
+      rel="noreferrer"
+    >
+      <div className="flex items-center gap-2 pr-2">
+        <img className="rounded-full" src={avatar} alt={username + ' avatar'} />
+        <div>
+          <p className="text-xl">{name}</p>
+          <p className="text-slate-600">@{username}</p>
+        </div>
+      </div>
+      <p className="p-2">{text}</p>
+    </a>
+  )
+}
+
+export default AutocompleteItem
