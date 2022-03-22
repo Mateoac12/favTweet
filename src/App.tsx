bd54e8bb4a3f9b82a8b483eea8fa943c24f4202f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-22 17:02:02 -0300

    feat: include routes, background and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Circles from './components/Circles'
+import Header from './components/Header'
+import { PublicRoutes } from './routes/index.routes'
+
 function App() {
-  return <div></div>
+  return (
+    <>
+      <Header />
+      <Circles />
+      <div className="max-w-screen-xl mx-auto min-h-screen">
+        <PublicRoutes />
+      </div>
+    </>
+  )
 }
 
 export default App
