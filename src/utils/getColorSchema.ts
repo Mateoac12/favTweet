09a86a6f4afc314b2c78e4b904f99d369b34c36f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-22 16:52:12 -0300

    feat: create a function to get a color schema

---
+++ src/utils/getColorSchema.ts
@@ -0,0 +1,13 @@
+import { IconPropsSVG } from '../types'
+
+type ColorProp = IconPropsSVG['color']
+
+export const getColorSchema = (color = 'slate' as ColorProp) => {
+  const colorSchema = {
+    sky: 'text-sky-500',
+    indigo: 'text-indigo-500',
+    slate: 'text-slate-400',
+  }
+
+  return colorSchema[color!]
+}
