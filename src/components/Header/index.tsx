00852201f5f240088aad6b4cdbef483c22ecd7d3
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-23 12:24:56 -0300

    feat: include login button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import Login from '../Login'
 import Title from '../Title'
 
 const Header = () => {
   return (
     <header className="fixed w-full backdrop-blur-sm bg-white/90 left-1/2 -translate-x-1/2 max-w-screen-xl px-4 py-2 mx-auto my-2 rounded-lg shadow-2xl shadow-indigo-50 z-40">
-      <div className="">
+      <div className="flex justify-between items-center">
         <Title />
+        <Login />
       </div>
     </header>
   )
