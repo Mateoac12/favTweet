da966c77ee5fa7b67705ce645ac0fe921b4b0c13
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-23 12:25:19 -0300

    feat: add white color

--- src/utils/getColorSchema.ts
+++ src/utils/getColorSchema.ts
@@ -7,6 +7,7 @@ export const getColorSchema = (color = 'slate' as ColorProp) => {
     sky: 'text-sky-500',
     indigo: 'text-indigo-500',
     slate: 'text-slate-400',
+    white: 'text-white',
   }
 
   return colorSchema[color!]
