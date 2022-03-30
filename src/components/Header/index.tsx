90d453e1a7bf22d5b828518537d6a62cbec673b1
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:36:53 -0300

    ref: swap title component to logo component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import Login from '../Login'
-import Title from '../Title'
+import Logo from '../Logo'
 
 const Header = () => {
   return (
     <header className="fixed w-full backdrop-blur-sm bg-white/90 left-1/2 -translate-x-1/2 max-w-screen-xl px-4 py-2 mx-auto my-2 rounded-lg shadow-2xl shadow-indigo-50 z-40">
       <div className="flex justify-between items-center">
-        <Title />
+        <Logo />
         <Login />
       </div>
     </header>
