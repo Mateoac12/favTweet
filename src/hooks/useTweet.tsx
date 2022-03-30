eb7a30d538b30789c4ae400bbab33baf8dd344e7
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:30:39 -0300

    feat: control tweets for the panel to add

---
+++ src/hooks/useTweet.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, useContext, useState } from 'react'
+import { TweetsContext } from '../context/TweetsContext'
+import { getTweet } from '../services/getTweet'
+import { checkTweetLink } from '../utils/checkTweetLink'
+import { useError } from './useError'
+
+export const useTweet = () => {
+  const { tweet, setTweet } = useContext(TweetsContext)
+  const [input, setInput] = useState<string>('')
+  const { error, setError, resetError } = useError()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleSetInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+  const handleCheckLink = async (link: string) => {
+    const { tweetId, pass, error } = checkTweetLink(link)
+
+    if (!pass) {
+      return setError({
+        message: error,
+        type: 'error',
+      })
+    }
+
+    resetError()
+
+    setIsLoading(true)
+    const { tweetData } = await getTweet({ tweetId } as { tweetId: string })
+
+    setTweet(tweetData)
+    tweetData && setIsLoading(false)
+  }
+
+  const setCategory = (categoty: string[]) => {
+    setTweet((lastData) => ({
+      ...lastData!,
+      category: Array(
+        ...new Set([...(lastData!.category?.concat(categoty) as string[])])
+      ),
+    }))
+  }
+
+  return {
+    input,
+    error,
+    isLoading,
+    tweet,
+    handleSetInput,
+    handleCheckLink,
+    setCategory,
+  }
+}
