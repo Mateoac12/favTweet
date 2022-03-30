36bb9f9c5f73a420ca765aa5ea3276641d3aeaaf
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:30:04 -0300

    feat: add button to go dashboard if logged

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,17 @@
+import { useLocation } from 'wouter'
 import Button from '../components/Button'
 import Search from '../components/Search'
 import TargetTweet from '../components/TargetTweet'
+import Title from '../components/Title'
 import TwitterIllustrationHome from '../components/TwitterIllustrationHome/Index'
 import Underline from '../components/Underline'
+import { useHandleLogin } from '../hooks/useHandleLogin'
 import { tweets } from '../mock/data'
 
 const LandingPage = () => {
+  const { user } = useHandleLogin()
+  const [, setLocation] = useLocation()
+
   return (
     <>
       <section className="h-screen flex">
@@ -21,18 +27,26 @@ const LandingPage = () => {
             cuando quieras.
           </h1>
           <div className="flex gap-4">
-            <Button size="text-2xl">Empezar</Button>
-            <Button size="text-2xl" type="secondary">
-              Información
-            </Button>
+            {user ? (
+              <Button onClick={() => setLocation('/dashboard')} size="text-2xl">
+                Ir a tus tweets
+              </Button>
+            ) : (
+              <>
+                <Button size="text-2xl">Empezar</Button>
+                <Button size="text-2xl" type="secondary">
+                  Información
+                </Button>
+              </>
+            )}
           </div>
         </div>
         <TwitterIllustrationHome />
       </section>
       <section className="my-12 p-8 bg-white/20 rounded-lg shadow-2xl shadow-indigo-50">
-        <h1 className="text-6xl font-semibold mb-4">
+        <Title size="text-6xl">
           Conserva tus <Underline color="indigo">tweets</Underline>
-        </h1>
+        </Title>
         <Search data={tweets} sourceSearchId="landing-page-example" />
         <section className=" mb-12 my-4 masonry sm:masonry-sm md:masonry-md">
           {tweets.map((tweet) => (
