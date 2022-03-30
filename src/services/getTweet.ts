6db8ba6dc16494d79990351a44c6c8b08c73dde2
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:29:10 -0300

    feat: get tweets from external api

---
+++ src/services/getTweet.ts
@@ -0,0 +1,41 @@
+import { ITargetTweet, ITweetAPI } from '../types'
+
+interface Props {
+  tweetId: string
+}
+
+export const getTweet = async ({ tweetId }: Props) => {
+  const url = import.meta.env.VITE_SERVICE_GET_TWEET as string
+
+  const tweetData: ITargetTweet = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ tweetId }),
+  })
+    .then((res) => res.json())
+    .then((res: ITweetAPI) => {
+      const { data, includes } = res
+
+      const userData = includes.users[0]
+      const tweetData = data[0]
+
+      return {
+        avatar: userData.profile_image_url,
+        createdAt: tweetData.created_at,
+        name: userData.name,
+        username: userData.username,
+        description: userData.description,
+        followers: userData.public_metrics.followers_count,
+        following: userData.public_metrics.following_count,
+        isProtect: userData.protected,
+        text: tweetData.text,
+        tweetId: tweetData.id,
+        userId: userData.id,
+        verified: userData.verified,
+        category: [],
+      }
+    })
+
+  return {
+    tweetData,
+  }
+}
