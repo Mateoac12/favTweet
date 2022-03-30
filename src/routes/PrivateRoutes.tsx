dd66e6523f5d636bbf8cfa911542462aa27c6af0
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:29:21 -0300

    feat: add private routes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,5 +1,13 @@
-const PrivateRoutes = () => {
-  return <></>
-}
+import { Redirect, Route, Switch } from 'wouter'
+import Dashboard from '../pages/Dashboard'
+import LandingPage from '../pages/LandingPage'
 
-export default PrivateRoutes
+export const PrivateRoutes = () => {
+  return (
+    <Switch>
+      <Route path="/" component={LandingPage} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Redirect to="/dashboard" />
+    </Switch>
+  )
+}
