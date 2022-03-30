de8132e5243862853469071350e86ce524376aaa
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-30 19:29:40 -0300

    feat: create index of routes

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,1 +1,9 @@
-export { PublicRoutes } from './PublicRoutes'
+import { PublicRoutes } from './PublicRoutes'
+import { PrivateRoutes } from './PrivateRoutes'
+import { useHandleLogin } from '../hooks/useHandleLogin'
+
+export const Routes = () => {
+  const { user } = useHandleLogin()
+
+  return <>{user ? <PrivateRoutes /> : <PublicRoutes />}</>
+}
