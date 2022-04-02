2924d446675cfd68715d2e4b731d3d22494dd484
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-01 22:57:16 -0300

    geat: get tweets for user

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,15 +1,28 @@
-import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+} from 'firebase/firestore'
 import { ITargetTweet, IUserProfile } from '../types'
 
 const db = getFirestore()
 
 const addTweet = async (tweet: ITargetTweet, userId: IUserProfile['uid']) => {
-  const newTweet = await addDoc(
-    collection(db, `${userId}/tweets/${tweet.tweetId}`),
-    tweet
-  )
-  console.log(tweet)
+  const newTweet = await addDoc(collection(db, `${userId}`), tweet)
   return newTweet
 }
 
-export { addTweet }
+const getTweets = (
+  userId: IUserProfile['uid'],
+  callback: (result: ITargetTweet[]) => void
+) => {
+  const collRef = collection(db, `${userId}`)
+
+  onSnapshot(collRef, ({ docs }) => {
+    const results = docs.map((doc) => doc.data() as ITargetTweet)
+    callback(results)
+  })
+}
+
+export { addTweet, getTweets }
