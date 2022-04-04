45a063b9e5891a898bc5798d8b1b30d6ccbf4718
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-04 20:27:38 -0300

    feat: apply search, filter and btn to add tweets

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react'
+import AddTweetPanel from '../components/AddTweetPanel'
+import FilterList from '../components/FilterList'
+import Search from '../components/Search'
 import TargetTweet from '../components/TargetTweet'
 import { getTweets } from '../firebase/firestore'
 import { useHandleLogin } from '../hooks/useHandleLogin'
@@ -8,7 +11,15 @@ import NonTweets from './NonTweets'
 const Dashboard = () => {
   const [tweets, setTweets] = useState<ITargetTweet[]>([])
   const [loader, setLoader] = useState<boolean>(true)
+  const [categories, setCategories] = useState<string[]>([])
   const { user } = useHandleLogin()
+
+  useEffect(() => {
+    const tweetsCategories = [
+      ...new Set(tweets.map((tweet) => tweet.category).flat()),
+    ]
+    setCategories(tweetsCategories as string[])
+  }, [tweets])
 
   useEffect(() => {
     getTweets(user?.uid!, (results: ITargetTweet[]) => {
@@ -26,12 +37,21 @@ const Dashboard = () => {
 
   if (tweets.length > 0)
     return (
-      <div className="absolute mt-28">
+      <div className="max-w-screen-xl min-h-screen mx-auto pt-28">
+        <h1 className="text-2xl text-slate-600">Busca tus tweets</h1>
+        <Search data={tweets} sourceSearchId="dashboard-tweets" />
+        <div className="flex items-center justify-start gap-4 mb-4">
+          <h1 className="text-xl text-slate-600">Filtrar</h1>
+          <FilterList buttonName="Categorías" list={categories} />
+        </div>
         <section className="my-4 mb-12 masonry sm:masonry-sm md:masonry-md">
           {tweets.map((tweet) => (
             <TargetTweet key={`target-dashboard-${tweet.tweetId}`} {...tweet} />
           ))}
         </section>
+        <div className="fixed -translate-x-1/2 bottom-4 left-1/2">
+          <AddTweetPanel existCategories={categories} />
+        </div>
       </div>
     )
 
