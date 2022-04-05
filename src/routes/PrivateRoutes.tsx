7fa7a1d09c10a2487662b2515496b0d91a11ce62
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-05 20:58:09 -0300

    feat: add tweets provider

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,4 +1,5 @@
 import { Redirect, Route, Switch } from 'wouter'
+import TweetsProvider from '../context/TweetsContext'
 import Dashboard from '../pages/Dashboard'
 import LandingPage from '../pages/LandingPage'
 
@@ -6,8 +7,10 @@ export const PrivateRoutes = () => {
   return (
     <Switch>
       <Route path="/" component={LandingPage} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Redirect to="/dashboard" />
+      <TweetsProvider>
+        <Route path="/dashboard" component={Dashboard} />
+        <Redirect to="/dashboard" />
+      </TweetsProvider>
     </Switch>
   )
 }
