10918e56cc68ca75cdb097f3ba0f93706667c1b7
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-12 22:29:07 -0300

    feat: add skelleton

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import AddTweetPanel from '../components/AddTweetPanel'
 import FilterList from '../components/FilterList'
+import Placeholder from '../components/Placeholder'
 import Search from '../components/Search'
 import TargetTweet from '../components/TargetTweet'
 import { getTweets } from '../firebase/firestore'
@@ -33,8 +34,20 @@ const Dashboard = () => {
 
   if (loader)
     return (
-      <div className="absolute mt-28">
-        <h1>loading</h1>
+      <div className="max-w-screen-xl min-h-screen mx-auto mb-40 pt-28">
+        <div className="flex flex-col gap-4">
+          <Placeholder id="text-search" w={200} h={28} />
+          <Placeholder id="search" w={1200} h={38} />
+        </div>
+        <div className="flex gap-4 mt-4">
+          <Placeholder id="text-category" w={80} h={28} />
+          <Placeholder id="category" w={100} h={28} />
+        </div>
+        <div className="flex gap-4 mt-8">
+          <Placeholder id="tweet-target-1" w={400} h={320} rounded={'md'} />
+          <Placeholder id="tweet-target-2" w={460} h={260} rounded={'md'} />
+          <Placeholder id="tweet-target-3" w={300} h={340} rounded={'md'} />
+        </div>
       </div>
     )
 
