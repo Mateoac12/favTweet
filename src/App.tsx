b23471e49044e9571a788a0c7ba1ffb35d862d4f
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-18 22:28:54 -0300

    fix: remove experimental changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Circles from './components/Circles'
 import Header from './components/Header'
 import TweetProvider from './context/TweetContext'
 import UserProvider from './context/UserContext'
-import { Routes } from './routes/Routes'
+import { Routes } from './routes/index.routes'
 
 function App() {
   return (
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,7 +2,6 @@ import { Switch, Route, Redirect } from 'wouter'
 import LandingPage from '../pages/LandingPage'
 
 export const PublicRoutes = () => {
-  console.log('public page')
   return (
     <>
       <Switch>
