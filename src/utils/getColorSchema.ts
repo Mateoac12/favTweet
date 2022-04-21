d228c149dd29f150bdea921fb3f4d77b5730ce60
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-21 20:25:01 -0300

    test: apply new test

--- src/utils/getColorSchema.ts
+++ src/utils/getColorSchema.ts
@@ -1,14 +1,8 @@
+import { colorSchemaForSVG } from '../config/colorSchemaForSVG'
 import { IconPropsSVG } from '../types'
 
 type ColorProp = IconPropsSVG['color']
 
 export const getColorSchema = (color = 'slate' as ColorProp) => {
-  const colorSchema = {
-    sky: 'text-sky-500',
-    indigo: 'text-indigo-500',
-    slate: 'text-slate-400',
-    white: 'text-white',
-  }
-
-  return colorSchema[color!]
+  return colorSchemaForSVG[color!] || colorSchemaForSVG.slate
 }
