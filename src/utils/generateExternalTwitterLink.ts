88eb950fa1ec403493ed8a804c706108ec17cb39
Mateo Álvarez <alvarez.fing@gmail.com>
2022-04-21 20:25:08 -0300

    test: apply new test

--- src/utils/generateExternalTwitterLink.ts
+++ src/utils/generateExternalTwitterLink.ts
@@ -6,6 +6,12 @@ interface Props {
 }
 
 export const generateExternalTwitterLink = ({ username, tweetId }: Props) => {
+  const tweetIdNotNumber = isNaN(Number(tweetId))
+  if (tweetIdNotNumber) return `https://twitter.com/${username}`
+
+  const hasUsername = username.length > 0
+  if (!hasUsername) return 'https://twitter.com/'
+
   const link = `https://twitter.com/${username}/status/${tweetId}`
 
   return link
